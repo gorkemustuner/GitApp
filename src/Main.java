b27dc0c3c78cf492a51bc0297b22901dc3ce9d12
ustunergorkem <gorkem.ustuner@misli.com>
2025-01-03 22:25:45 +0300

    v1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //versiyon 2
+        System.out.println("Değişiklik 2");
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //versiyon 1
+        System.out.println("Değişiklik 1");//ws
+
+
+
+
+
+
+    }
+}
